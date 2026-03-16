63836b9c6568a4d6631f077c4475c31c08e560d6
Aditya Trivedi <me@aditya.io>
2026-03-16 15:21:44 -0700

    feat(nvim): add lazy.nvim with catppuccin theme
    Bootstrap lazy.nvim plugin manager on startup and configure
    catppuccin (macchiato) as the default colorscheme.
    
    Includes lazy-lock.json to pin plugin versions.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,32 @@
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- core options
 require('config.options')
+
+-- plugins
+require('lazy').setup({
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      flavour = 'macchiato',
+    },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd.colorscheme('catppuccin')
+    end,
+  },
+})
 
 -- keymaps: VS Code vs standalone
 if vim.g.vscode then
